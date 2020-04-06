8895cd0ea4a6c5acdc73e444a9862b33b29fa1ca
phoiniks <phoiniks@grellopolis.de>
2020-04-06 11:19:27 +0200

    Funktionsweise anders als erwartet...

--- expectations.pl
+++ expectations.pl
@@ -1,60 +1,53 @@
 #!/usr/bin/perl
-use strict;
-use warnings;
-use utf8;
-use Encode qw( decode encode );
-use Data::Dumper;
+use Modern::Perl;
 use diagnostics;
-use autodie;
+use autodie qw( :all );
 use Hash::Ordered;
 use Expect;
-use Redis;
+use Log::Log4perl;
 
-my $rds = Redis->new;
-$rds->select(1);
+Log::Log4perl::init( 'log4perl.conf' );
+
+my $log = Log::Log4perl->get_logger();
+
+$log->info( "BEGINN" );
 
 my $exp = Expect->new;
 
-# my @grellus = glob("*.txt");
-
-# for ( @grellus ) {
-#     unlink $_;
-# }
-
 $exp->log_file("EXPECTATIONS.LOG", "w");
 
-
 my $parameters = Hash::Ordered->new(
-    'Bezeichnung'     => 'Softwareentwickler Perl etc.',
-    'Firma'           => 'Grellopolis GmbH & Co. KG',
-    'Anrede'          => 'Herr',
-    'Ansprechpartner' => 'Grellopoulos',
-    'Strasse'         => 'Kühnehöfe 25',
-    'Ort'             => '22761 Hamburg',
-    'Gehalt'          => '60000',
-    'Quelle'          => 'stepstone.de',
-    'Telefon'         => '040 80 60 37 99',
-    'Email'           => 'andreas.grellopoulos\@grellopolis.de',
-    'Angebotstext'    => 'grellopolis.txt'
+    'Bezeichnung: '     => 'Softwareentwickler Perl etc.',
+    'Firma: '           => 'Grellopolis GmbH & Co. KG',
+    'Anrede: '          => 'Herr',
+    'Ansprechpartner: ' => 'Grellopoulos',
+    'Straße: '          => 'Kühnehöfe 25',
+    'Ort: '             => '22761 Hamburg',
+    'Gehalt: '          => '60000',
+    'Quelle: '          => 'stepstone.de',
+    'Telefon: '         => '040 80 60 37 99',
+    'E-Mail: '          => 'andreas.grellopoulos@grellopolis.de',
+    'Angebotstext: '    => 'grellopolis.txt',
     );
 
 my @prompts = $parameters->keys;
 
-print "@prompts\n";
-
-$exp->exp_internal(1);
+$exp->exp_internal( 1 );
 
 my @values = $parameters->values;
 
-$exp->spawn("Brief.pl TEMPLATE_ENTWICKLUNG.tt", @values);
+$exp->spawn("./Brief.pl", "TEMPLATE_ENTWICKLUNG.tt");
 
 for ( @prompts ) {
-    print $_, "\n";
-    $exp->expect(1, $_);
+    $log->debug( $_ );
     my $value = $parameters->get($_);
-    print $value, "\n";
-    $exp->send($value . "\n");
+    $exp->send( $value . "\r" );
+    $exp->expect( 1, $_ );
+    $log->debug( $value );
+    $exp->debug( 3 );
     $exp->exp_continue;
 }
 
-$exp->soft_close();
+$log->info( "ENDE" );
+
+$exp->hard_close();
