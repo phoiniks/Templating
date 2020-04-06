0b34c67a66af0c4f811b9852880ffb2299d2c2e5
phoiniks <phoiniks@grellopolis.de>
2020-04-06 16:10:51 +0200

    Erste Version

---
+++ YAMLizer.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use Modern::Perl;
+use diagnostics;
+use autodie qw( :all );
+use Hash::Ordered;
+use Expect;
+use Log::Log4perl;
+use utf8;
+use YAML::XS qw( DumpFile );
+
+Log::Log4perl::init( 'log4perl.conf' );
+
+my $log = Log::Log4perl->get_logger();
+
+$log->info( "BEGINN" );
+
+my $parameters = Hash::Ordered->new(
+    'Bezeichnung: '     => 'Bezeichnung',
+    'Firma: '           => 'Firma',
+    'Anrede: '          => 'Anrede',
+    'Ansprechpartner: ' => 'Ansprechpartner',
+    'Straße: '          => 'Straße',
+    'Ort: '             => 'Ort',
+    'Gehalt: '          => 'Gehalt',
+    'Quelle: '          => 'Quelle',
+    'Telefon: '         => 'Telefon',
+    'E-Mail: '          => 'E-Mail',
+    'Angebotstext: '    => 'Angebotstext',
+    );
+
+my $yaml = DumpFile( "PARAMETERS.YAML", $parameters);
