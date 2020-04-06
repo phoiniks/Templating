a7f988ef028dcbb6fa7cb9a7b0d62e984fc467d9
phoiniks <phoiniks@grellopolis.de>
2020-04-06 16:41:59 +0200

    Unbedeutende Änderungen

--- Brief.pl
+++ Brief.pl
@@ -1,66 +1,69 @@
 #!/usr/bin/perl
 use Modern::Perl;
 use autodie;
-use String::TT qw( tt strip );
+use String::TT qw( tt );
 use POSIX qw( strftime );
+use Carp qw( croak );
 use Log::Log4perl;
 
 Log::Log4perl::init( 'log4perl.conf' );
 
 my $log = Log::Log4perl->get_logger();
 
-my $lokalzeit              = strftime "%A_%d_%B_%Y_%H:%M:%S", localtime;
+my $lokalzeit             = strftime "%A_%d_%B_%Y_%H:%M:%S", localtime;
 
 $log->info( "BEGINN" );
 
 $log->info( $lokalzeit );
 
 print "Bezeichnung: ";
-chomp(my $bezeichnung      = <STDIN>);
+chomp(my $bezeichnung     = <STDIN>);
 
 print "Firma: ";
-chomp(my $firma            = <STDIN>);
+chomp(my $firma           = <STDIN>);
 
-$firma =~ s/\&/\\&/g;
+$firma                    =~ s/\&/\\&/g;
+
+$log->debug( $firma );
 
 print "Anrede: ";
-chomp(my $anrede           = <STDIN>);
+chomp(my $anrede          = <STDIN>);
 
 print "Ansprechpartner: ";
-chomp(my $ansprechpartner  = <STDIN>);
+chomp(my $ansprechpartner = <STDIN>);
 
 print "Straße: ";
-chomp(my $strasse          = <STDIN>);
+chomp(my $strasse         = <STDIN>);
 
 print "Ort: ";
-chomp(my $ort              = <STDIN>);
+chomp(my $ort             = <STDIN>);
 
 print "Gehalt: ";
-chomp(my $gehalt           = <STDIN>);
+chomp(my $gehalt          = <STDIN>);
 
 print "Quelle: ";
-chomp(my $quelle           = <STDIN>);
+chomp(my $quelle          = <STDIN>);
 
 print "Telefon: ";
-chomp(my $telefon          = <STDIN>);
+chomp(my $telefon         = <STDIN>);
 
 print "E-Mail: ";
-chomp(my $email            = <STDIN>);
+chomp(my $email           = <STDIN>);
 
 print "Angebotstext: ";
-chomp(my $angebot          = <STDIN>);
+chomp(my $angebot         = <STDIN>);
 
 local $/;
 open my $datei, "<", $ARGV[0];
 
 $log->debug( $ARGV[0] );
 
-my $vorlage                = <$datei>;
+my $vorlage            = <$datei>;
 
 close $datei;
 
 
-my ($firma_kurz) = split / /, $firma, -1;
+my ($firma_kurz)       = split / /, $firma, -1;
 
 $log->debug( sprintf "FIRMA_KURZ: %s", $firma_kurz );
 
@@ -68,7 +71,7 @@ my ($bezeichnung_kurz) = split / /, $bezeichnung, -1;
 
 $log->debug( sprintf "BEZEICHNUNG_KURZ: %s", $bezeichnung_kurz );
 
-my $ausgabedatei           = $bezeichnung_kurz . "_" . $firma_kurz . "_" . $lokalzeit . ".tex";
+my $ausgabedatei       = $bezeichnung_kurz . "_" . $firma_kurz . "_" . $lokalzeit . ".tex";
 
 $log->debug( $ausgabedatei );
 
@@ -76,12 +79,13 @@ open my $out, ">", $ausgabedatei;
 
 print $out sprintf "%s", tt $vorlage;
 
-close $out;
+my $output = `pdflatex $ausgabedatei`
+    or croak "Was ist hier los? $!";
 
-my $output = `pdflatex $ausgabedatei`;
+close $out;
 
 $log->debug( $output );
 
 $log->info( "ENDE" );
 
-exit;
+ exit;
