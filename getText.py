16bfc5ac2a0d4a9d27ebbd603d622e44dde3b88c
phoiniks <phoiniks@grellopolis.de>
2020-08-11 15:20:04 +0200

    Der Versuch einer semantischen Wichtung des Textinventars

--- getText.py
+++ getText.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from collections import Counter
+import math
 from sys import argv
 from time import sleep
 from nltk.tokenize import TreebankWordTokenizer
@@ -25,12 +26,28 @@ with open(argv[1]) as datei:
     lexikon_bereinigt = [lexem for lexem in lexikon if regex.match("[a-zA-ZäöüÄÖÜ]", lexem)]
 
     zaehler = Counter(lexikon_bereinigt)
-    haeufig = zaehler.most_common(10)
+    frequenz = zaehler.most_common()
 
     lexikon = list(enumerate(lexikon_bereinigt))
 
     # Nur zur Kontrolle, wird ersetzt durch Redis-, MongoDB- und/oder SQL-Anbindung:
-    
+
+    print("\nLänge des Textes: {}\n".format(len(lexikon)))
+
     print(lexikon)
 
-    print(haeufig)
+    print()
+    
+    print("=" * 150)
+
+    print()
+
+    print("\nLänge des Lexikons: {}\n\n".format(len(frequenz)))    
+
+    drittel = math.floor(len(frequenz) / 3)
+
+    print("Mittleres Drittel beginn bei Lemma {}.\n".format(drittel * 2))
+    
+    print("Das mittlere Drittel (der Frequenzanalyse gemäß der semantisch relevanteste Teil des Textes):\n")
+
+    print(frequenz[drittel:(drittel * 2)])
